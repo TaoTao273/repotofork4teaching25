bf48650eea74d70bc1cfa2f5b4330c79f8700627
Tanja Leyerer <tanja.leyerer@gebhart.org>
2025-12-05 20:17:06 +0100

    Add FinalStory Nikolaus to helloWorldTL.m

---
+++ helloWorldTL.m
@@ -0,0 +1,99 @@
+% function helloWorldTL()
+    % helloWorldTL.m - Übungsdatei für Git & MATLAB
+    % Autor: TL
+
+    disp('Hello World from TL!');
+end
+
+
+% =====================================================
+% Nikolaus-Story (finale Version von TL)
+% =====================================================
+%
+%           /\_/\
+%          ( o.o )    ← Octo-Nikolaus
+%          o_> ^ <_o
+%          (_ ___ _)
+%            U  U
+%
+%
+% Ho Ho Ho!
+%
+%
+% getting evil 😈...
+%
+%
+%🎁🎁🎁  ← klaut Geschenke
+%
+%
+%                /\_/\
+%      _____     ( .\/.)    
+%  _____        o_> ^ <_o
+%     ____      (_ ___ _)
+%                 U  U
+%   
+% HoHoHo 
+%oder auch nicht ...
+% 
+%     
+%
+%
+%
+%   
+%       /\_/\     ✋    😇    😇   😇
+%      ( 0.0 )    ✋    /|\   /|\   /|\
+%      _> ^ <_    ✋    / \   / \   / \
+%     (_W__ W_)
+%       U  U                   
+%
+%              STOP!  
+%        
+%       
+%    Drei Engel halten Nikolaus auf!
+%
+%
+%
+%           /\_/\        _________
+%           ( O.O )    ___________
+%          ._> ^ <_.      ____________
+%          (_ ___ _)   
+%            U   U
+%
+%
+%
+% Doch irgendwann passt Nikolaus nicht auf…
+%
+%        
+%                    😱
+%          /\_/\
+%      o  ( x.x )  o     ← Nikolaus stürzt!
+%       °(       )°  
+%      |    U-U
+%      |           |
+%      |           |
+%      |           |
+%     /    GRUBE    \
+%    /               \
+%   /                 \
+%  /                   \        
+% /                     \
+%/____|__ ___________|___\
+%
+%
+%
+%
+%
+% Ein Weihnachtswunder ist geschehen..
+%
+%
+%*    *          *
+%   *    *   *     *
+%            *
+%     *    *    *
+%  *   *            *   
+%*   *         *   
+%        *
+%* 🎁 🎁 🎁   *
+%
+%
+% Die Geschenke sind alle wieder zurück!
